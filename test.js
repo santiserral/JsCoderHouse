7cd5b554a369c56a0a33ef2db5490d9c39566999
chanti <89088194+santiserral@users.noreply.github.com>
2022-10-26 22:38:44 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,7 @@
+// let numeroIngresado = prompt("Ingresa un número");
+// let salida = numeroIngresado*2
+// alert(salida);
+const pi = 3.1415
+let radio = prompt("Ingrese su numero")
+let salida = pi*radio**2
+alert(salida);
