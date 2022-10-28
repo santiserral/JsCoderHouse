56e0be021e57b7ae4007c21e469cb4a5d5b206e3
chanti <89088194+santiserral@users.noreply.github.com>
2022-10-27 22:00:52 -0300

    PERDON EMI DISCULPAME PORFAVOR emi:no

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // let numeroIngresado = prompt("Ingresa un número");
 // let salida = numeroIngresado*2
 // alert(salida);
-const pi = 3.1415
-let radio = prompt("Ingrese su numero")
-let salida = pi*radio**2
-alert(salida);
+const pi = 4
+let radio = 5.3
+let salida = pi*radio**2 
+console.log('Total', salida);
