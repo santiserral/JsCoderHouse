05214e6d1d997685569353dc6bb03a5447e5476a
chanti <89088194+santiserral@users.noreply.github.com>
2022-10-31 22:09:41 -0300

    agrego maria y maria belen

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 let nombreIngresado = prompt("Ingrese su nombre");
-if((nombreIngresado !="") && ((nombreIngresado == "belen") || (nombreIngresado == "belibu") || (nombreIngresado == "belibuwu"))){
+if((nombreIngresado !="") && ((nombreIngresado == "belen") || (nombreIngresado == "belibu") || (nombreIngresado == "belibuwu") || (nombreIngresado == "maria") || (nombreIngresado == "maria belen"))){
 alert("Hola mi amor te amo mucho sos hermosisima y preciosa sabelo");
 }
 else{
